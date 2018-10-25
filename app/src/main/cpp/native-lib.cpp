afac37d3b5730be92fdd3743d600be45038f2c55
guidongyuan <guidongyuan@163.com>
2018-10-25 21:04:40 +0800

    实现增量更新

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,12 +1,26 @@
 #include <jni.h>
-#include <string>
 
-extern "C" JNIEXPORT jstring
+// 执行合并差分包，实际上就是bspatch.c中的main()方法
+// 由于native-lib.cpp为c++，bspatch.c为c，所以需要使用extern
+extern "C" {
+extern int main(int argc,char * argv[]);
+}
 
-JNICALL
-Java_cn_guidongyuan_studypatch_MainActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+extern "C"
+JNIEXPORT void JNICALL
+Java_cn_guidongyuan_studypatch_MainActivity_native_1bspatch(JNIEnv *env, jobject instance,
+                                                            jstring oldApk_, jstring newApk_,
+                                                            jstring patchFile_) {
+    const char *oldApk = env->GetStringUTFChars(oldApk_, 0);
+    const char *newApk = env->GetStringUTFChars(newApk_, 0);
+    const char *pathFile = env->GetStringUTFChars(patchFile_, 0);
+
+    char * argv[4] = {"", const_cast<char *>(oldApk), const_cast<char *>(newApk),
+                      const_cast<char *>(pathFile)};
+
+    main(4, argv);
+
+    env->ReleaseStringUTFChars(oldApk_, oldApk);
+    env->ReleaseStringUTFChars(newApk_, newApk);
+    env->ReleaseStringUTFChars(patchFile_, pathFile);
 }
--- app/src/main/java/cn/guidongyuan/studypatch/MainActivity.java
+++ app/src/main/java/cn/guidongyuan/studypatch/MainActivity.java
@@ -1,11 +1,22 @@
 package cn.guidongyuan.studypatch;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Environment;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
@@ -15,15 +26,65 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        TextView textView = findViewById(R.id.sample_text);
+        textView.setText(BuildConfig.VERSION_NAME);
     }
 
     /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
+     * 更新
      */
-    public native String stringFromJNI();
+    public void update(View view) {
+        new AsyncTask<Void,Void,File>() {
+
+            @Override
+            protected File doInBackground(Void... voids) {
+                // 获取当前应用的路径
+                String oldPath = getApplication().getApplicationInfo().sourceDir;
+                String newPath = Environment.getExternalStorageDirectory().getPath() + File.separator + "new.apk";
+                String patchPath = Environment.getExternalStorageDirectory().getPath() + File.separator + "patch.diff";
+
+                if (new File(patchPath).exists()) {
+                    native_bspatch(oldPath,newPath,patchPath);
+                    return new File(newPath);
+                }else {
+                    Log.w(TAG, "patch file is not exists");
+                    return null;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(File file) {
+                if (file != null) {
+                    installNewApk(file);
+                }
+            }
+        }.execute();
+    }
+
+    /**
+     * 跳转到安装页面
+     * @param file 新包路径
+     */
+    private void installNewApk(File file) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            intent.setDataAndType(Uri.fromFile(file), "application/vnd.android.package-archive");
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        }else {
+            // 声明需要的临时权限
+            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            String packageName = getApplication().getPackageName();
+            Uri contentUri = FileProvider.getUriForFile(MainActivity.this, packageName + ".fileProvider", file);
+            intent.setDataAndType(contentUri, "application/vnd.android.package-archive");
+        }
+        startActivity(intent);
+    }
+
+    /**
+     * 合并差分包
+     * @param oldApk 当前项目路径
+     * @param newApk 差分包路径
+     * @param patchFile 合并后新包路径
+     */
+    private native void native_bspatch(String oldApk,String newApk,String patchFile);
 }
